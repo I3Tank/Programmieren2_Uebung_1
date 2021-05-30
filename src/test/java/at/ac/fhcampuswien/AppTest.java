41f880593c0b073cf0e5d55557926db3e3d93f35
I3Tank <64980879+I3Tank@users.noreply.github.com>
2021-05-30 23:59:33 +0200

    all the Tests

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -15,11 +15,37 @@ public class AppTest {
     cant have other special symbols
      */
 
-
     @Test
     public void checkLengthTest(){
         myPassword password = new myPassword("validLengthPassword");
         boolean actual = password.checkPasswordLength();
         assertTrue(actual);
     }
+
+    @Test
+    public void checkUpperLowerTest(){
+        myPassword password = new myPassword("ValidUpperLowerPassword");
+        boolean actual = password.checkUpperLower();
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkNumberTest_singleNumber(){
+        myPassword password = new myPassword("ValidNumberPassword1");
+        boolean actual = password.checkNumber();
+        assertTrue(actual);
+    }
+    @Test
+    public void specialSymbolsNeededTest(){
+        myPassword password = new myPassword("ValidSymbolsPassword$");
+        boolean actual = password.checkSymbolsNeeded();
+        assertTrue(actual);
+    }
+    @Test
+    public void checkNoOtherSymbolsTest(){
+        myPassword password = new myPassword("ValidSymbolsPassword$!");
+        boolean actual = password.checkNoOtherSymbols();
+        assertTrue(actual);
+    }
+
 }
