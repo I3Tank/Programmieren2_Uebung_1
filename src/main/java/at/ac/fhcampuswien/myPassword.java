91d4bb6fc528e917785023790ea1cd7e68194aea
I3Tank <64980879+I3Tank@users.noreply.github.com>
2021-05-31 01:02:55 +0200

    Part 1 done

--- src/main/java/at/ac/fhcampuswien/myPassword.java
+++ src/main/java/at/ac/fhcampuswien/myPassword.java
@@ -1,7 +1,8 @@
 package at.ac.fhcampuswien;
+import java.lang.*;
 
 public class myPassword {
-    private String password;
+    private final String password;
 
     public myPassword(String password){
         this.password = password;
@@ -9,5 +10,49 @@ public class myPassword {
 
     public boolean checkPasswordLength(){
         return password.length() > 8 && 25 > password.length();
+    }
+
+    public boolean checkUpperLower(){
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))){
+                hasUpper = true;
+            }
+            if (Character.isLowerCase(password.charAt(i))){
+                hasLower = true;
+            }
+            if (hasUpper && hasLower){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkNumber(){
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkSymbolsNeeded(){
+        for (int i = 0; i < password.length(); i++) {
+            if (password.matches(".*[()#$?!%/@].*")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkNoOtherSymbols(){
+        for (int i = 0; i < password.length(); i++) {
+            if (password.matches(".*[/*^&\"{}_\\[\\]|\\<>,.].*")){
+                return false;
+            }
+        }
+        return true;
     }
 }
