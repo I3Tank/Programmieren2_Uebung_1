d4d0ca8d3da10d8741c3cce10f1057909f66f9dc
I3Tank <64980879+I3Tank@users.noreply.github.com>
2021-05-30 23:36:27 +0200

    Initial commit

---
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -0,0 +1,25 @@
+package at.ac.fhcampuswien;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+
+
+    /*
+    password 8-25 Chars
+    Uppercase/Lowercase needed
+    Numbers needed
+    ()#$?!%/@ needed
+    cant have other special symbols
+
+
+     */
+    @Test
+    public void checkLengthTest(){
+        myPassword password = new myPassword("validLengthPassword");
+        boolean actual = password.checkPasswordLength(password);
+        assertTrue(actual);
+    }
+}
